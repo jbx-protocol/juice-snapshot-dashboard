fe1b1e57849766441c412ca331a5da9baccec37f
aeolian <aeolian.eth@protonmail.com>
2021-12-23 20:57:53 +1000

    Add chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,5 +1,16 @@
 import useProposals from "../hooks/Proposals";
 import useScores from "../hooks/Scores";
+import { useMemo } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 export default function Chart({
   space,
@@ -16,44 +27,92 @@ export default function Chart({
     tokenSymbol,
     proposals: proposals ?? [],
   });
-  if (!proposals || !scores || Object.keys(scores || {}).length === 0)
+
+  const chartData = useMemo(() => {
+    if (!proposals || !scores || Object.keys(scores || {}).length === 0) {
+      return;
+    }
+    return proposals.map((p: any, i: any) => {
+      const yesVotes = p.votes.filter((p: any) => p.choice === 1);
+      const noVotes = p.votes.filter((p: any) => p.choice === 2);
+      const abstainVotes = p.votes.filter((p: any) => p.choice === 3);
+      const yesVotesTokens = yesVotes.reduce((sum: number, vote: any) => {
+        return sum + scores[p.id][vote.voter];
+      }, 0);
+      const noVotesTokens = noVotes.reduce((sum: number, vote: any) => {
+        return sum + scores[p.id][vote.voter];
+      }, 0);
+      const abstainVotesTokens = abstainVotes.reduce(
+        (sum: number, vote: any) => {
+          return sum + scores[p.id][vote.voter];
+        },
+        0
+      );
+
+      return {
+        idx: i,
+        titleShort: p.title.split(" - ")[0],
+        title: p.title,
+        id: p.id,
+        yesVotes,
+        noVotes,
+        abstainVotes,
+        yesVotesTokens,
+        noVotesTokens,
+        abstainVotesTokens,
+      };
+    });
+  }, [proposals, scores]);
+
+  if (!chartData) {
     return <div>Loading...</div>;
+  }
+
+  const CustomTooltip = ({
+    active,
+    payload,
+  }: {
+    active?: any;
+    payload?: any;
+  }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{`${payload[0].payload.title}`}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
-    <div>
-      {proposals.map((p: any) => {
-        const yesVotes = p.votes.filter((p: any) => p.choice === 1);
-        const noVotes = p.votes.filter((p: any) => p.choice === 2);
-        const abstainVotes = p.votes.filter((p: any) => p.choice === 3);
-
-        const yesVotesTokens = yesVotes.reduce((sum: number, vote: any) => {
-          return sum + scores[p.id][vote.voter];
-        }, 0);
-        const noVotesTokens = noVotes.reduce((sum: number, vote: any) => {
-          return sum + scores[p.id][vote.voter];
-        }, 0);
-        const abstainVotesTokens = abstainVotes.reduce(
-          (sum: number, vote: any) => {
-            return sum + scores[p.id][vote.voter];
-          },
-          0
-        );
-
-        return (
-          <div key={p.id}>
-            <h2>{p.title}</h2>
-            <div>
-              {yesVotesTokens} JBX ({yesVotes.length} votes) Yes
-            </div>
-            <div>
-              {noVotesTokens} JBX ({noVotes.length}) No votes
-            </div>
-            <div>
-              {abstainVotesTokens} JBX ({abstainVotes.length}) Abstain votes
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <ResponsiveContainer width="90%" height="70%">
+      <BarChart
+        width={500}
+        height={300}
+        data={chartData}
+        margin={{
+          top: 5,
+          right: 50,
+          left: 50,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey="titleShort"
+          angle={-45}
+          textAnchor="end"
+          interval={0}
+          offset={1000}
+        />
+        <YAxis label={{ value: "JBX", angle: -90 }} offset={100} />
+        <Tooltip content={<CustomTooltip />} />
+        <Legend verticalAlign="top" />
+        <Bar dataKey="yesVotesTokens" fill="#1E88E5" />
+        <Bar dataKey="noVotesTokens" fill="#D81B60" />
+      </BarChart>
+    </ResponsiveContainer>
   );
 }
