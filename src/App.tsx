5a8a01fa6f4eaa88763d167c32fe1870f191ea47
aeolian <aeolian.eth@protonmail.com>
2021-12-24 21:04:31 +1000

    Fix logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import "./App.css";
 import ProposalsChart from "./components/ProposalsChart";
+import logo from "./assets/logo.png"; // Tell webpack this JS file uses this image
 
 function App() {
   return (
     <div className="App">
       <header>
         <a href="https://juicebox.money" rel="noopener noreferrer">
-          <img height={40} src="./snapshot-analytics/assets/logo.png" alt="Juicebox logo" />
+          <img height={40} src={logo} alt="Juicebox logo" />
         </a>
       </header>
       <main>
         <h1>JuiceboxDAO active proposals</h1>
-        <div style={{ width: "80%", height: "80%", margin: '0 auto' }}>
+        <div style={{ width: "80%", height: "80%", margin: "0 auto" }}>
           <ProposalsChart
             space="jbdao.eth"
             tokenSymbol="JBX"
